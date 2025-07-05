c5d7748c2bbe2a5e2d1442eae1bbcd9e89092c30
Cong0707 <49234756+Cong0707@users.noreply.github.com>
2025-07-06 00:34:11 +0800

    实现防毒附魔

--- src/main/com/evermc/everisland/fabric/acidwater/Acid.kt
+++ src/main/com/evermc/everisland/fabric/acidwater/Acid.kt
@@ -4,36 +4,40 @@ import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.api.ModInitializer
 import net.minecraft.block.Block
-import net.minecraft.block.Blocks
-import net.minecraft.block.FluidBlock
+import net.minecraft.enchantment.Enchantment
 import net.minecraft.fluid.FlowableFluid
-import net.minecraft.item.BucketItem
 import net.minecraft.item.Item
-import net.minecraft.item.Items
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
 
 
 @Environment(EnvType.SERVER)
 class Acid : ModInitializer {
     companion object {
+        val namespace = "acid"
+
         lateinit var ACID: FlowableFluid
         lateinit var FLOWING_ACID: FlowableFluid
         lateinit var ACID_BUCKET: Item
         lateinit var ACID_BLOCK: Block
+        lateinit var ANTI_ACID: RegistryKey<Enchantment>
     }
 
     override fun onInitialize() {
-        ACID = Registry.register(Registries.FLUID, Identifier.of("acid", "acid"), AcidFluid.Still())
-        FLOWING_ACID = Registry.register(Registries.FLUID, Identifier.of("acid", "flowing_acid"), AcidFluid.Flowing())
+        ACID = Registry.register(Registries.FLUID, Identifier.of(namespace, "acid"), AcidFluid.Still())
+        FLOWING_ACID = Registry.register(Registries.FLUID, Identifier.of(namespace, "flowing_acid"), AcidFluid.Flowing())
         ACID_BUCKET = Registry.register(
-            Registries.ITEM, Identifier.of("acid", "acid_bucket"),
+            Registries.ITEM, Identifier.of(namespace, "acid_bucket"),
             AcidBucketItem()
         )
         ACID_BLOCK = Registry.register(
             Registries.BLOCK,
-            Identifier.of("acid", "acid_block"),
+            Identifier.of(namespace, "acid_block"),
             AcidFluidBlock())
+
+        ANTI_ACID = RegistryKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(namespace, "anti_acid"))
     }
 }
--- src/main/com/evermc/everisland/fabric/acidwater/AcidFluidBlock.kt
+++ src/main/com/evermc/everisland/fabric/acidwater/AcidFluidBlock.kt
@@ -5,9 +5,37 @@ import eu.pb4.polymer.core.api.block.PolymerBlock
 import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
 import net.minecraft.block.FluidBlock
+import net.minecraft.enchantment.Enchantments
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class AcidFluidBlock: FluidBlock(ACID, Settings.copy(Blocks.WATER)), PolymerBlock {
     override fun getPolymerBlockState(p0: BlockState?): BlockState {
         return Blocks.WATER.getStateWithProperties(p0)
+    }
+
+    override fun onEntityCollision(state: BlockState?, world: World, pos: BlockPos?, entity: Entity) {
+        if (entity is PlayerEntity) {
+            var damage = 1.0f
+
+            listOf(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET).forEach { slot ->
+                val armor = entity.getEquippedStack(slot)
+                if (!armor.isEmpty) {
+                    if (armor.hasEnchantments()) {
+                        armor.enchantments.enchantments.map { it.key.get() }.find { it == Acid.ANTI_ACID }?.run {
+                            if (world.random.nextInt(400) == 0) {
+                                armor.damage(1, entity, slot)
+                            }
+                            damage -= 0.25f
+                        } ?: return@forEach
+                    }
+                }
+            }
+
+            entity.damage(world.damageSources.magic(), damage)
+        }
     }
 }
