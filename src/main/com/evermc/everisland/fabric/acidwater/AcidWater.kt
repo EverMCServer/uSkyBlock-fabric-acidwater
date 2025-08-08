53c1452cb5eb2811d4ab178102cc53f77dac948b
GJ <740646495@qq.com>
2025-08-09 03:20:36 +0800

    调试unbreaking等级获取 2

--- src/main/com/evermc/everisland/fabric/acidwater/AcidWater.kt
+++ src/main/com/evermc/everisland/fabric/acidwater/AcidWater.kt
@@ -92,16 +92,13 @@ class AcidWater : ModInitializer {
                         if (player.world.random.nextInt(n) == 0) {
                             // Damage one random antiacid armor 1 durability
                             val (armor, slot) = antiAcidArmors.random()
-                            // unbreaking enchantment check
-                            if (armor.hasEnchantments()) {
-                                // Get unbreaking level of the armor
-                                val unbreaking = armor.enchantments.enchantments.find { it.key==UNBREAKING }?.let{ armor.enchantments.getLevel(it) }?:0
-                                print("Unbreaking = $unbreaking")
-                                if (player.world.random.nextInt(unbreaking+1)==0) {
-                                    // Damage armor
-                                    armor.damage(1, player, slot)
-                                }
-                            }
+                            // Get unbreaking level of the armor
+                            val unbreaking = armor.enchantments.enchantments.find { it.value()==UNBREAKING }?.let{ armor.enchantments.getLevel(it) }?:0
+                            armor.damage(unbreaking+1, player, slot)
+//                            if (player.world.random.nextInt(unbreaking+1)==0) {
+//                                // Damage armor
+//                                armor.damage(1, player, slot)
+//                            }
                         }
                     }
                     dmgTick[uuid] = Pair(10, false) // Reset ticks after damage
